d677594162d0f231eef140dbadfb9064329eb8e8
MaxTyson27 <sirotkinmax58@icloud.com>
2020-12-01 18:42:44 +0300

    added shop page

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,42 @@
 $(function () {
+
+  $('.shop-content__filter-btn').on('click', function () {
+    $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
+    $(this).addClass('shop-content__filter-btn--active');
+  });
+
+  $('.button-list').on('click', function () {
+    $('.products-gallery__item').addClass('products-gallery--list')
+  });
+
+  $('.button-grid').on('click', function () {
+    $('.products-gallery__item').removeClass('products-gallery--list')
+  });
+
+
+  $(".star").rateYo({
+    starWidth: "11px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
+  });
+
+  $('.select-style').styler();
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data){
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  });
+
+
   $('.slider__inner').slick({
     arrows: false,
     dots: true,
